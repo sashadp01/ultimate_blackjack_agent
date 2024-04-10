522e123b8beb07ceee11c3cfa43a385909974e10
Sasha Denouvilliez-Pech <sasha.denouvilliez-pech@mail.mcgill.ca>
2024-04-10 19:55:41 -0400

    preliminary hp tuning

--- train_graphs_v0.py
+++ train_graphs_v0.py
@@ -1,32 +1,65 @@
-import gymnasium as gym
-import numpy as np
-from stable_baselines3 import PPO, A2C
+from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import VecMonitor
 from stable_baselines3.common.monitor import Monitor
-import torch
 from BlackJack_Simulator.BlackJack import UltimateBlackjackRoundEnv
-from stable_baselines3.common.callbacks import BaseCallback, EvalCallback
-from statistics import mean
+from stable_baselines3.common.callbacks import EvalCallback
+from utils.utils import *
 
-testing = False
+EPISODE_LENGTH = 2
+N_ENVS = 1
+
+monitor_kwargs = {
+    "info_keywords": (
+        "illegal",
+        "won",
+        "hit",
+        "stand",
+        "double",
+        "surrender",
+        "split",
+        "return",
+    )
+}
+env_kwargs = {"card_counting": True}
+model_name = "test_lr_linear_n_env_1"
 
 
-# def learning_rate_schedule(progress_remaining):
-#     start_lr = 0.0003
-#     end_lr = 0.000001
-#     return start_lr - ((1-progress_remaining) * (start_lr - end_lr))
-def learning_rate_schedule(progress_remaining):
+def lr_fixed_schedule(progress_remaining):
+    return 0.0003
+
+
+def lr_step_schedule(progress_remaining):
+    """
+    Learning rate step schedule as explained in the paper
+    http://cs230.stanford.edu/projects_fall_2021/reports/103066753.pdf
+    """
+    if 1 - progress_remaining < (1e4 / (1e4 + 1e6 + 1e7)):
+        return 0.0001
+    elif 1 - progress_remaining < (1e6 / (1e4 + 1e6 + 1e7)):
+        return 0.00001
+    else:
+        return 0.000001
+
+
+def lr_linear_schedule(progress_remaining):
+    start_lr = 0.0003
+    end_lr = 0.000001
+    return start_lr - ((1 - progress_remaining) * (start_lr - end_lr))
+
+
+def lr_decay_schedule(progress_remaining):
     start_lr = 0.0003
     decay_rate = 1000
-    return start_lr / (1 + decay_rate * (1-progress_remaining))
-
-monitor_kwargs = {"info_keywords": ("illegal", "won", "hit", "stand", "double", "surrender", "split","return")}
-env_kwargs = {"card_counting": False}
+    return start_lr / (1 + decay_rate * (1 - progress_remaining))
 
 
 # Define the parallel environments
-vec_env = make_vec_env(UltimateBlackjackRoundEnv, n_envs=4, monitor_kwargs=monitor_kwargs,env_kwargs=env_kwargs)
+vec_env = make_vec_env(
+    UltimateBlackjackRoundEnv,
+    n_envs=N_ENVS,
+    monitor_kwargs=monitor_kwargs,
+    env_kwargs=env_kwargs,
+)
 
 
 # Train the agent
@@ -35,54 +68,12 @@ model = PPO(
     vec_env,
     verbose=0,
     device="cpu",
-    learning_rate=learning_rate_schedule, # To be fine tuned
+    learning_rate=lr_linear_schedule,  # To be fine tuned
+    # batch_size=32,
+    # n_steps=8192,
+    # n_epochs=5,
     tensorboard_log="./ppo_UBJ_tensorboard/",
 )
-
-
-class TensorboardCallback(BaseCallback):
-    """
-    Custom callback for plotting additional values in tensorboard.
-    """
-
-    def __init__(self, verbose=0):
-        super().__init__(verbose)
-
-    def _on_step(self) -> bool:
-        # Log scalar value (here a random variable)
-        illegal = []
-        won = []
-        hit = []
-        stand = []
-        double = []
-        surrender = []
-        split = []
-        return_ = []
-        
-        for idx, info in enumerate(self.locals["infos"]):
-            if info.get("episode") is not None:
-                episode = info["episode"]
-                illegal.append(episode["illegal"])
-                won.append(episode["won"])
-                hit.append(episode["hit"])
-                stand.append(episode["stand"])
-                double.append(episode["double"])
-                surrender.append(episode["surrender"])
-                split.append(episode["split"])
-                return_.append(episode["return"])
-                
-        
-        if len(illegal) > 0:
-            self.logger.record("action_log/illegal", mean(illegal))
-            self.logger.record("action_log/won", mean(won))
-            self.logger.record("action_log/hit", mean(hit))
-            self.logger.record("action_log/stand", mean(stand))
-            self.logger.record("action_log/double", mean(double))
-            self.logger.record("action_log/surrender", mean(surrender))
-            self.logger.record("action_log/split", mean(split))
-            self.logger.record("action_log/return", mean(return_))
-             
-        return True
 
 
 eval_env = UltimateBlackjackRoundEnv(**env_kwargs)
@@ -94,7 +85,21 @@ eval_callback = EvalCallback(
     deterministic=True,
     render=False,
     n_eval_episodes=10000,
+    verbose=0,  # For no output
 )
 
-model.learn(total_timesteps=10000000, progress_bar=True, log_interval=1, callback=[TensorboardCallback(), eval_callback])
-model.save("ppo_UBJ")
+model.learn(
+    # Would multiplying by N_ENVS * EPISODE_LENGTH be the "best" way to compare trainig with the paper?
+    # Training time explodes if we do that
+    total_timesteps=(1e4 + 1e6 + 1e7),
+    # total_timesteps=(1e6),
+    progress_bar=True,
+    log_interval=1,
+    callback=[TensorboardCallback(), eval_callback],
+    tb_log_name=model_name,
+)
+
+model.save(model_name)
+
+# Try CnnPolicy
+# Try
